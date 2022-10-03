6cd323d5b80533adf56504e077064192d0128df3
MysticShadow427 <devendrakayande427@gmail.com>
2022-10-03 17:45:31 +0530

    socket.io basics implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express=require('express')
+const http=require('http')
+const socketio=require('socket.io')
 const path=require('path')
 
 const app=express()
+const server=http.createServer(app)
+const io=socketio(server);
 
 const port=process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port,()=>{
+io.on('connection',()=>{
+    console.log('Client connected :)')
+})
+
+server.listen(port,()=>{
     console.log(`Server running on ${port}`)
 })
