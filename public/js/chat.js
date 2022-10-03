3f6137cc522fe24136f55267a2e661ebff12c965
MysticShadow427 <devendrakayande427@gmail.com>
2022-10-03 22:00:54 +0530

    events and emits

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io() 
+const socket=io()
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+document.querySelector('#mf').addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const message=e.target.elements.message
+    socket.emit('sendMessage',message)
+})
