de511584f82f03054995925f62510c7aafd44408
MysticShadow427 <devendrakayande427@gmail.com>
2022-10-04 17:41:16 +0530

    Acknowledgement Code added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,46 @@
 const socket=io()
 
+const $messageForm=document.querySelector('#mf')
+const $messageFormInput=$messageForm.querySelector('input')
+const $messageFormButton=$messageForm.querySelector('button')
+const $sendLocationButton=document.querySelector('#sl')
+
+
 socket.on('message',(message)=>{
     console.log(message)
 })
 
-document.querySelector('#mf').addEventListener('submit',(e)=>{
+$messageForm.addEventListener('submit',(e)=>{
     e.preventDefault()
 
-    const message=e.target.elements.message
-    socket.emit('sendMessage',message)
+    $messageFormButton.setAttribute('disabled','disabled')
+
+    const Message=e.target.elements.message
+    socket.emit('sendMessage',Message,(error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value=''
+        $messageFormInput.focus()
+
+        if(error) console.log(error)
+
+        console.log('Delivered Successfcully')
+    })
+})
+
+$sendLocationButton.addEventListener('click',()=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation Not Supported')
+    }
+
+    $sendLocationButton.setAttribute('disabled','disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        },()=>{
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location Shared')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const http=require('http')
 const socketio=require('socket.io')
 const path=require('path')
+const Filter=require('bad-words')
 
 const app=express()
 const server=http.createServer(app)
@@ -16,9 +17,26 @@ io.on('connection',(socket)=>{
     console.log('Client connected :)')
 
     socket.emit('message','Welcome ! :)')
+    socket.broadcast.emit('message','A new user has joined')
 
-    socket.on('sendMessage',(message)=>{
-        io.emit('message',message)
+    socket.on('sendMessage',(Message,callback)=>{
+        const filter=new Filter()
+        if(filter.isProfane(Message)){
+            return callback('Profanity not allowed')
+        }
+
+        io.emit('message',Message)
+        callback('Delivered');
+    })
+
+    socket.on('sendLocation',(coords,callback)=>{
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+
+        callback()
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user disconnected')
     })
 })
 
